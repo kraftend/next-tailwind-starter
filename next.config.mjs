8678201478a0be5eb44fadbf0536083c3f93291c
cemreinanc <cemreinanc@gmail.com>
2022-10-15 19:14:25 +0300

    feat: :sparkles: new practices, fixes and dependency updates

---
+++ next.config.mjs
@@ -0,0 +1,57 @@
+// @ts-check
+// eslint-disable-next-line unused-imports/no-unused-imports
+import { env } from './src/env/server.mjs';
+
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+];
+
+export default defineNextConfig({
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+  poweredByHeader: false,
+});
---
+++ src/env/schema.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+import { z } from 'zod';
+
+/**
+ * Specify your server-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ */
+export const serverSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']),
+  VERCEL: z.boolean().default(false),
+});
+
+/**
+ * Specify your client-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
+ */
+export const clientSchema = z.object({
+  // example:
+  // NEXT_PUBLIC_ROOT_DOMAIN: z.string().min(1),
+});
+
+/**
+ * You can't destruct `process.env` as a regular object, so you have to do
+ * it manually here. This is because Next.js evaluates this at build time,
+ * and only used environment variables are included in the build.
+ * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
+ */
+export const clientEnv = {
+  // example:
+  // NEXT_PUBLIC_ROOT_DOMAIN: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+};
+
+/**
+ * You can't destruct `process.env` as a regular object in Next.js Middleware also,
+ * so you have to do it manually here. Also it helps to parse and set conditional defaults.
+ * @type {{ [k in keyof z.infer<typeof serverSchema>]: z.infer<typeof serverSchema>[k] | undefined }}
+ */
+export const serverEnv = {
+  NODE_ENV: process.env.NODE_ENV,
+  VERCEL: ['1', 'true', 'TRUE'].includes(process.env.VERCEL ?? ''),
+};
